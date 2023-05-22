72cf28c0414e5ecc5bcce132802a7bda1e9d65be
feyyaz başer <feyaz12@hotmail.com>
2023-05-22 14:02:07 +0300

    Add project files.

---
+++ RabbitMQ-Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://lhzvwhvx:6hVBInuK-klWodBsrXQLqyDhIC-tmNuV@rat.rmq2.cloudamqp.com/lhzvwhvx");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false); //durable=>rabbitmq refresh olunca kuyruk kalbolmasın memoryde tutmaya yarar
+                                                         //exclusive=>true olursa sadece burdaki kanaldan erişebliriz,autodelete=>son subscriber düşerse kuyruk otomatik silinir
+
+string message = "Welcome to RabbitMQ";
+var messageBody = Encoding.UTF8.GetBytes(message); // RabbitMQ ya mesajlar byte[] olarak gönderilir
+
+channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+Console.WriteLine("Mesaj RabbitMQye gönderildi");
+Console.ReadLine();
---
+++ RabbitMQ-Subscriber/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://lhzvwhvx:6hVBInuK-klWodBsrXQLqyDhIC-tmNuV@rat.rmq2.cloudamqp.com/lhzvwhvx");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//channel.QueueDeclare("hello-queue", true, false, false); //durable=>rabbitmq refresh olunca kuyruk kalbolmasın memoryde tutmaya yarar
+                                                         //exclusive=>true olursa sadece burdaki kanaldan erişebliriz,autodelete=>son subscriber düşerse kuyruk otomatik silinir
+
+
+var consumer =new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true, consumer);  // autoack=>true olursa kuyruktan siler false yaparsak doğru okunduktan sonra silmesini biz söyleriz
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Mesajınız: " + message);
+
+};
+
+
+Console.ReadLine();
